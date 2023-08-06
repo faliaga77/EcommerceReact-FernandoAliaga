bd4af7f5949961912402d103d599bb21d386f745
Fernando Aliaga <faliaga00@gmail.com>
2023-08-06 12:41:14 -0300

    retoque de css

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,3 +1,8 @@
+import Container from 'react-bootstrap/Container';
 export const ItemListContainer = (props) => {
-    return <div>{props.greeting}</div>;
+    return (
+    <Container className='mt-4'>
+       <h1>{props.greeting}</h1>
+    </Container>
+    );
 };
