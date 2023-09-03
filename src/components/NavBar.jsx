2ab909561c4296b5919b77513f22b7ffb64449ef
Fernando Aliaga <faliaga00@gmail.com>
2023-09-03 10:02:02 -0300

    correciones ecommerce

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,23 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
-
 import { CartWidget } from "./CartWidget";
+import {NavLink} from "react-router-dom";
+
 export const NavBar = () => (<Navbar bg="dark" data-bs-theme="dark">
   <Container>
-    <Navbar.Brand href="#home">La Tienda de Ana</Navbar.Brand>
+    <Navbar.Brand as={NavLink} to="/">La Tienda de Ana</Navbar.Brand>
     <Nav className="me-auto">
-      <Nav.Link href='/'>Home</Nav.Link>
-      <Nav.Link href="/category/tupperware">Tupperware</Nav.Link>
-      <Nav.Link href="/category/bazar">Bazar</Nav.Link>
-      <Nav.Link href="/category/cosmetica">Cosmetica</Nav.Link>
+      <Nav.Link as={NavLink} to='/'>Home</Nav.Link>
+      <Nav.Link as={NavLink} to="/category/tupperware">Tupperware</Nav.Link>
+      <Nav.Link as={NavLink} to="/category/bazar">Bazar</Nav.Link>
+      <Nav.Link as={NavLink} to="/category/cosmetica">Cosmetica</Nav.Link>
     </Nav>
     <CartWidget />
   </Container>
 </Navbar>
 
 );
+
+
+
