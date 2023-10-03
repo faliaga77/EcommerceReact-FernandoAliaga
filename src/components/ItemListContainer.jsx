f530ecc59561c7fcaee1b949750d7f9fc4f9c02a
Fernando Aliaga <faliaga00@gmail.com>
2023-10-03 19:05:40 -0300

    borrado de json del proyecto

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,3 +1,49 @@
+// import React, { useState, useEffect } from "react";
+// import { useParams } from "react-router-dom";
+// import Container from "react-bootstrap/Container";
+// import { ItemList } from "./ItemList";
+// import { getFirestore, getDocs, collection, query, where } from "firebase/firestore";
+
+// export const ItemListContainer = (props) => {
+//   const [products, setProducts] = useState([]);
+//   const [loading, setLoading] = useState(true);
+
+//   const { id } = useParams();
+
+//   useEffect(() => {
+//     const db = getFirestore();
+
+//     const refCollection = id
+//       ? query(collection(db, "ItemCollection"), where("categoryid", "==", id)) 
+//       : collection(db, "ItemCollection");
+
+//     getDocs(refCollection)
+//       .then((snapshot) => {
+//         if (snapshot.size === 0) setProducts([]);
+//         else {
+//           setProducts(
+//             snapshot.docs.map((doc) => ({
+//               id: doc.id,
+//               ...doc.data(),
+//             }))
+//           );
+//         }
+//       })
+//       .finally(() => setLoading(false)); 
+//   }, [id]);
+
+//   if (loading) return <div>Loading...</div>;
+
+//   return (
+//     <Container className="container-style">
+//       <h1>Productos</h1>
+//       <div className="carProducts">
+//         <ItemList products={products} />
+//       </div>
+//     </Container>
+//   );
+// };
+
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Container from "react-bootstrap/esm/Container";
@@ -5,45 +51,75 @@ import { ItemList } from "./ItemList";
 import { getFirestore, getDocs, collection, query, where } from "firebase/firestore";
 
 export const ItemListContainer = props => {
-    const [products, setProducts] = useState([])
-    const [loading, setLoading] = useState(true)
+  const [products, setProducts] = useState([])
+  const [loading, setLoading] = useState(true)
 
-    const { id } = useParams()
+  const { id } = useParams()
 
-    useEffect(() => {
-        const db = getFirestore();
+  //     useEffect(() => {
+  //         const db = getFirestore();
 
-        const refCollection = collection(db, "ItemCollection")
+  //         const refCollection = collection(db, "ItemCollection")
 
-        const q = id ? query(refCollection, where("categoryid", "==", id)) : refCollection;
-        
+  //         const q = id ? query(refCollection, where("categoryid", "==", id)) : refCollection;
 
-        getDocs(q)
-           .then(snapshot => {
-            // if (snapshot.size === 0) console.log("no results")
-            setProducts(
-                    snapshot.docs.map(doc  => {
-                        return { productid: doc.id, ...doc.data() }
-                    })
-                )
-        })
-        .finally(() => {
-            setLoading(false)
-        })
-    }, [id])
 
-     if (loading) return <div>Loading...</div>;
+  //         getDocs(q)
+  //            .then(snapshot => {
+  //             // if (snapshot.size === 0) console.log("no results")
+  //             setProducts(
+  //                     snapshot.docs.map(doc  => {
+  //                         return { productid: doc.id, ...doc.data() }
+  //                     })
+  //                 )
+  //         })
+  //         .finally(() => {
+  //             setLoading(false)
+  //         })
+  //     }, [id])
 
-return (
+  //      if (loading) return <div>Loading...</div>;
+
+  // return (
+
+  //     <Container className="container-style">
+  //         <h1>Productos</h1>
+  //         <div className="carProducts">
+  //             <ItemList products={products} />
+  //         </div>
+  //     </Container>
+  // );
+  // };
+
+  useEffect(() => {
+    const db = getFirestore();
+    const q = id ? query(collection(db, "ItemCollection"), where("categoryid", "==", id)) : collection(db, "ItemCollection");
+
+
+    getDocs(q)
+      .then(snapshot => {
+        // if (snapshot.size === 0) console.log("no results")
+        setProducts(
+          snapshot.docs.map(doc => {
+            return { productid: doc.id, ...doc.data() }
+          })
+        )
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }, [id])
+
+  if (loading) return <div>Loading...</div>;
+
+  return (
 
     <Container className="container-style">
-        <h1>Productos</h1>
-        <div className="carProducts">
-            <ItemList products={products} />
-        </div>
+      <h1>Productos</h1>
+      <div className="carProducts">
+        <ItemList products={products} />
+      </div>
     </Container>
-);
+  );
 };
-
 export default ItemListContainer;
-
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,30 +1,22 @@
+import React from 'react';
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { CartWidget } from "./CartWidget";
-import {NavLink} from "react-router-dom";
-
-import data from "../data/products.json";
-
-const categories = data.map(product => product.category)
-const uniqueCategories = new Set(categories)
+import Nav from 'react-bootstrap/Nav';
+import { NavLink } from 'react-router-dom';
+import { CartWidget } from './CartWidget';
 
 export const NavBar = () => (
- <Navbar bg="dark" data-bs-theme="dark">
-  <Container>
-    <Navbar.Brand href='#home'>La Tienda de Ana</Navbar.Brand>
-    <Nav className="me-auto">
-      <Nav.Link href='/'>Home</Nav.Link>
-       {[...uniqueCategories].map(category => (
-        <Nav.Link as={NavLink} key={category} to={`/category/${category}`} >
-       {category}
-       </Nav.Link>
-       ))}
-    </Nav>
-    <CartWidget/> 
-  </Container>
-</Navbar>
+    <Navbar bg="dark" variant="dark">
+        <Container>
+            <Navbar.Brand as={NavLink} to="/">La Tienda de Ana</Navbar.Brand>
 
+            <Nav className="me-auto">
+                <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+                <Nav.Link as={NavLink} to="/categoryid/tupperware">Tupperware</Nav.Link>
+                <Nav.Link as={NavLink} to="/categoryid/bazar">Bazar</Nav.Link>
+                <Nav.Link as={NavLink} to="/categoryid/cosmetica">Cosmetica</Nav.Link>
+            </Nav>
+            <CartWidget />
+        </Container>
+    </Navbar>
 );
-
-
